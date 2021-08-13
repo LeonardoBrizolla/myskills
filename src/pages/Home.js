680e338147b4bbe33b3ab60fda7b04c56a4aff61
Leonardo Brizolla <leonardobrizollars@gmail.com>
2021-08-13 00:24:59 -0300

    A imutabilidade no React utilizando o useState

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { 
   View, 
   Text, 
@@ -8,6 +8,13 @@ import {
   Platform } from 'react-native';
 
 export function Home(){
+  const [newSkill, setNewSkill] = useState('');
+  const [mySkills, setMySkills] = useState([]);
+
+  function handleAddNewSkill(){
+    setMySkills(oldState => [...oldState, newSkill]);
+  }
+
   return (
       <View style={styles.container}>
         <Text style={styles.title}>
@@ -18,18 +25,30 @@ export function Home(){
           style={styles.input} 
           placeholder="New skill"
           placeholderTextColor="#555"
+          onChangeText={setNewSkill}
         />
 
         <TouchableOpacity 
           style={styles.button}
           activeOpacity={.8}
+          onPress={handleAddNewSkill}
         >
           <Text style={styles.buttonText}>Add</Text>
         </TouchableOpacity>
 
-        <Text style={[styles.title, { marginTop: 50 }]}>
+        <Text style={[styles.title, { marginVertical: 50 }]}>
           My Skills
         </Text>
+
+        {
+          mySkills.map(skill => (
+          <TouchableOpacity key={skill} style={styles.buttonSkill}>
+            <Text style={styles.textSkill}>
+              {skill}
+            </Text>
+          </TouchableOpacity>
+          ))
+        }
       </View>
   )
 }
@@ -64,6 +83,18 @@ const styles = StyleSheet.create({
   buttonText: {
     fontSize: 17,
     color: '#FFF',
+    fontWeight: 'bold',
+  },
+  buttonSkill: {
+    backgroundColor: '#1F1E25',
+    padding: 15,
+    borderRadius: 50,
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  textSkill: {
+    color: '#FFF',
+    fontSize: 22,
     fontWeight: 'bold',
   }
 })
